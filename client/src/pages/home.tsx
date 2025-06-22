1a908dd8bca031fb81d3499b44aec2a599da9d8a
Noor <nur.k@thearc.cc>
2025-06-22 20:48:18 +0600

    Move Trips logo and text to left corner of header

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -413,7 +413,7 @@ export default function Home() {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
               <div className="flex justify-between items-center h-16">
                 <div className="flex items-center space-x-2">
-                  <Logo className="text-white" size={32} />
+                  <Logo className="text-white" size={24} />
                   <h1 className="text-xl font-bold text-white">Trips</h1>
                 </div>
                 
